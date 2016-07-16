c17ff17ce8d22ef82c9335b9ca12e6d4ae8bd009
baipeng5 <baipeng@jd.com>
2016-07-16 09:33:42 +0800

    基础构建

---
+++ HttpTool.Core/Common/DefaultLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpTool.Core.Common
+{
+    public class DefaultLogger : ILogger
+    {
+
+        public void Infor(string log)
+        {
+            Console.WriteLine(string.Format("{0}[infor]:{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), log));
+        }
+
+        public void Error(string log)
+        {
+            Console.WriteLine(string.Format("{0}[error]:{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), log));
+        }
+
+        public void Error(string log, Exception ex)
+        {
+            if (ex == null)
+            {
+                Error(log);
+            }
+            else
+            {
+                Console.WriteLine(string.Format("{0}[error]:{1},exception:{2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), log, ex.Message));
+            }
+
+        }
+    }
+}
---
+++ HttpTool.Core/Model/BindHostHttpNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpTool.Core.Model
+{
+    public class BindHostHttpNode : HttpNode
+    {
+
+        /**
+         * 
+         *是否迭代，若迭代，将按照IP列表来执行后续的节点 
+         **/
+        public bool IsIteration { get; set; }
+
+
+        /**
+        * 
+        *获取IP列表的方法名称
+        **/
+        public string FunctionNameOfIPArray { get; set; }
+    }
+}
