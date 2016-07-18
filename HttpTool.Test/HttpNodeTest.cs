64087c0fc3d1634a6dc39c4c2f11d4cb96295640
baipeng5 <baipeng@jd.com>
2016-07-18 20:59:32 +0800

    增加测试用例

---
+++ HttpTool.Test/HttpNodeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HttpTool.Core.Model;
+using System.Collections.Generic;
+
+namespace HttpTool.Test
+{
+    [TestClass]
+    public class HttpNodeTest
+    {
+
+        static readonly SingleHttpFlow FLOW = new SingleHttpFlow();
+
+        static HttpNodeTest() {
+
+            FLOW.IncludeJSLib = new List<string>();
+            FLOW.IncludeJSLib.Add("jsLib\\jquery-1.12.3.min.js");
+          
+        }
+
+        [TestMethod]
+        public void HttpNodeGetTest()
+        {
+            HttpNode node1 = new HttpNode();
+            node1.RequestType = "get";
+            node1.ScriptOfHandleRequest = "function getUrl(){ return 'http://www.cnblogs.com/';};";
+            node1.ScriptOfHandleResponse = "alert(123);";
+            node1.FunctionNameOfRequestUrl = "getUrl";
+            FLOW.HeadNode = node1;
+            FLOW.Run(null);
+            
+        }
+
+
+    }
+}
